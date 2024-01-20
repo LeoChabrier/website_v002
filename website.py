a376de4a6a8201126c11d870e8458175568f9c27
Leo Chabrier <chabrierleo@outlook.fr>
2024-01-20 23:26:04 +0100

    update

--- website.py
+++ website.py
@@ -385,7 +385,7 @@ class Projects_Breakdowns():
             year_folder_path = path.join(PROJECTS_BREAKDOWNS, year_folder)
             sub_folders = next(walk(year_folder_path))[1]
 
-            sub_folders.reverse()
+            # sub_folders.reverse()
             
             year_subfolders_dict[year_folder] = sub_folders[::-1]
             all_subdirectories.extend(sub_folders)
