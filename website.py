4446f6ff79abd0237f6ab4ef465d7c7b11356d8a
Leo Chabrier <chabrierleo@outlook.fr>
2024-01-20 19:32:00 +0100

    update

--- website.py
+++ website.py
@@ -361,7 +361,7 @@ class Projects_Breakdowns():
             """
             <style>
             div.stButton > button {
-                width: 400px;
+                width: 370px;
             }
             </style>
             """,
